d02def552cb6a6ccfc484b0e0a60f62aea7b8937
shymans <austin.shymanski@gmail.com>
2013-10-22 17:44:31 -0400

    final copy with working bonus

--- excercise7.rb
+++ excercise7.rb
@@ -25,7 +25,23 @@ def increaseclass(math)
     calc = value * 0.05
     answer = value + calc
     puts "The new class size for #{key.to_s} is #{answer}"
+    math[key] = answer
   end
 end
 
 increaseclass(students)
+
+students.delete(:cohort2)
+
+studentcount(students)
+
+def addvalues(x)
+  sum = 0
+   x.each do |a, b|
+    sum = sum + b.to_i
+  end
+  return sum
+end
+
+puts addvalues(students)
+
