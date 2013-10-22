d40b1948f0f7f98c50afb2b50ebb661ee90fcc8b
austin shymanskigit config user.email austin.shymanski@gmail.com <Work@Austins-MacBook-Air.local>
2013-10-22 16:26:10 -0400

    printing initial cohorts

---
+++ excercise7.rb
@@ -0,0 +1,13 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def studentcount(cohort)
+  cohort.each do |key, value|
+    puts "#{key}: #{value} students"
+  end
+end
+
+studentcount(students)
